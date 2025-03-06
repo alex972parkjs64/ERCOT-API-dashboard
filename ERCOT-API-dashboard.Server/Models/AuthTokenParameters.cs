f6432357abe2538c3ce8cc6e0060496f986c2353
Alex Park <parkjs972@gmail.com>
2025-03-06 14:00:45 -0600

    refiningn AuthTokenParameters

--- ERCOT-API-dashboard.Server/Models/AuthTokenParameters.cs
+++ ERCOT-API-dashboard.Server/Models/AuthTokenParameters.cs
@@ -1,23 +1,68 @@
 ﻿namespace ERCOT_API_dashboard.Server.Models
 {
     public class AuthTokenParameters
-    {        
-        public required string UserName { get; set; }
-        public required string Password { get; set; }
+    {
+        private readonly IConfiguration _config;
+        private readonly string _token_username = "token:username";
+        private readonly string _token_password = "token:password";
+        private readonly string _token_api_subscription_key = "token:api_subscription_key";
+        private readonly string _token_scope = "token:scope";
+        private readonly string _token_client_id = "token:client_id";
+        private readonly string _grant_type = "password";
+        private readonly string _response_type = "id_token";
+
+        public AuthTokenParameters(IConfiguration config) 
+        {
+            _config = config;
+        }
+        public string UserName
+        { 
+            get
+            {
+                return _config[_token_username] ?? string.Empty;
+            }
+        }
+        public string Password 
+        { 
+            get
+            {
+                return _config[_token_password] ?? string.Empty;
+            }
+        }
+        public string ApiSubscriptionKey
+        {
+            get
+            {
+                return _config[_token_api_subscription_key] ?? string.Empty;
+            }
+        }
         public string GrantType
         {
             get
             {
-                return "password";
+                return _grant_type;
             }
         }
-        public required string Scope { get; set; }
-        public required string ClientId { get; set; }
+        public string Scope
+        {
+            get
+            {
+                return _config[_token_scope] ?? string.Empty;
+            }
+        }
+
+        public string ClientId
+        {
+            get
+            {
+                return _config[_token_client_id] ?? string.Empty;
+            }
+        }
         public string ResponseType
         {
             get
             {
-                return "id_token";
+                return _response_type;
             }
         }
 
