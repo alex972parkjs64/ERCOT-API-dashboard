b26ebf5eb82f6a00fc49f0fc2162d079d8f34f09
Alex Park <parkjs972@gmail.com>
2025-03-14 16:45:10 -0500

    progress save

--- ERCOT-API-dashboard.Server/Services/Interface/IErcotTokenService.cs
+++ ERCOT-API-dashboard.Server/Services/Interface/IErcotTokenService.cs
@@ -1,7 +1,9 @@
-﻿namespace ERCOT_API_dashboard.Server.Services.Interface
+﻿using ERCOT_API_dashboard.Server.Models;
+
+namespace ERCOT_API_dashboard.Server.Services.Interface
 {
     public interface IErcotTokenService
     {
-
+        Task<ErcotApiTokenResponse> GetErcotApiTokenAsync(AuthTokenParameters authTokenParameters, CancellationToken cancellationToken = default);
     }
 }
