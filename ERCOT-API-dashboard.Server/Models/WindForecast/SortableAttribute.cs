a3fdbab1f3fd1dc91a42432e6d5ef2719579a28c
Alex Park <parkjs972@gmail.com>
2025-07-10 17:04:27 -0500

    progress save

---
+++ ERCOT-API-dashboard.Server/Models/WindForecast/SortableAttribute.cs
@@ -0,0 +1,5 @@
+﻿namespace ERCOT_API_dashboard.Server.Models.WindForecast
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SortableAttribute : Attribute { }
+}
--- ERCOT-API-dashboard.Server/Models/WindForecast/SystemWideHourlyRegionalRequest.cs
+++ ERCOT-API-dashboard.Server/Models/WindForecast/SystemWideHourlyRegionalRequest.cs
@@ -1,23 +1,46 @@
 ﻿using ERCOT_API_dashboard.Server.Models.Interface;
 
+using System.Reflection;
+
 namespace ERCOT_API_dashboard.Server.Models.WindForecast
 {
     public record SystemWideHourlyRegionalRequest : IUrlParameters
     {
         private readonly DateTime DATE_TIME_NOT_SET = DateTime.MinValue;
         private readonly int INT_NOT_SET = -1;
+        private readonly string STR_NOT_SET = string.Empty;
         private readonly string _datetime_format = "yyyy-MM-ddTHH:mm:ss";
 
         public DateTime PostedFrom { get; init; }
         public DateTime PostedTo   { get; init; }
+        
+        [Sortable]
+        public string Region { get; init; }
+
+        [Sortable]
+        public string Model { get; init; }
 
         public int Page { get; init; }
         public int Size { get; init; }
+
+        // use this to validate Sort property later
+        internal bool ValidSortField(string sortParam)
+        {
+            var sortableProps = typeof(SystemWideHourlyRegionalRequest)
+                    .GetProperties()                    
+                    .Where(p => p.GetCustomAttribute<SortableAttribute>() != null)
+                    .Select(p => p.Name)
+                    .ToArray();
+
+            return sortableProps?.Contains(sortParam) ?? false;
+        }
 
         public SystemWideHourlyRegionalRequest() 
         {
             PostedFrom = DATE_TIME_NOT_SET;
             PostedTo   = DATE_TIME_NOT_SET;
+            Region = STR_NOT_SET;
+            Model = STR_NOT_SET;
             Size = INT_NOT_SET;
             Page = INT_NOT_SET;
         }
@@ -41,6 +64,22 @@ namespace ERCOT_API_dashboard.Server.Models.WindForecast
                     $"postedDatetimeTo={PostedTo.ToString(_datetime_format)}&"
                     :
                     string.Empty;
+            }
+        }
+
+        public string RegionParam
+        {
+            get
+            {
+                return Region != STR_NOT_SET ? $"region={Region}&" : string.Empty;
+            }
+        }
+
+        public string ModelParam
+        {
+            get
+            {
+                return Model != STR_NOT_SET ? $"model={Model}&" : string.Empty;
             }
         }
 
